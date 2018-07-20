69c5a729a58f973ea45cf9a9cc1249e681064da2
Phil Zona <phil.b.zona@gmail.com>
2018-07-20 10:24:46 -0500

    Add markdown editor to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Markdown from 'markdown-to-jsx';
-import logo from './logo.svg';
+
+// Import editor and some dependencies
+import AceEditor from 'react-ace';
+import brace from 'brace';
+import 'brace/mode/markdown';
+import 'brace/theme/dracula';
+
 import './App.css';
 
 const { ipcRenderer } = window.require('electron');
@@ -23,6 +29,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <AceEditor
+          mode="markdown"
+          theme="dracula"
+          onChange={newContent => {
+            this.setState({
+              loadedFile: newContent
+            });
+          }}
+          name="markdown_editor"
+          value={this.state.loadedFile}
+        />
         <Markdown>
           {this.state.loadedFile}
         </Markdown>
