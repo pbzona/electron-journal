9f5b8add6103238d01d3adeef294544b3238e97d
Phil Zona <phil.b.zona@gmail.com>
2018-07-20 14:15:53 -0500

    Basic app styles and simplified example file data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Markdown from 'markdown-to-jsx';
+import styled from 'styled-components';
 
 // Import editor and some dependencies
 import AceEditor from 'react-ace';
@@ -29,23 +30,59 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AceEditor
-          mode="markdown"
-          theme="dracula"
-          onChange={newContent => {
-            this.setState({
-              loadedFile: newContent
-            });
-          }}
-          name="markdown_editor"
-          value={this.state.loadedFile}
-        />
-        <Markdown>
-          {this.state.loadedFile}
-        </Markdown>
+        <Split>
+          <CodeWindow>
+            <AceEditor
+              mode="markdown"
+              theme="dracula"
+              onChange={newContent => {
+                this.setState({
+                  loadedFile: newContent
+                });
+              }}
+              name="markdown_editor"
+              value={this.state.loadedFile}
+            />
+          </CodeWindow>
+          <RenderedWindow>
+            <Markdown>{this.state.loadedFile}</Markdown>
+          </RenderedWindow>
+        </Split>
       </div>
     );
   }
 }
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+
+  h1, h2, h3, h4, h5, h6 {
+    color: #82d8d8;
+  };
+
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  };
+
+  a {
+    color: #e54b4b;
+  }
+`;
