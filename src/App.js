6d622f84cd9d0faff5d3f86c84c6547c91b45a79
Phil Zona <phil.b.zona@gmail.com>
2018-07-20 14:27:24 -0500

    Custom app header

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header>Journal</Header>
         <Split>
           <CodeWindow>
             <AceEditor
@@ -54,6 +55,21 @@ class App extends Component {
 }
 
 export default App;
+
+const Header = styled.header`
+  background-color: #191324;
+  color: #75717c;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.4);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`;
 
 const Split = styled.div`
   display: flex;
--- src/main.js
+++ src/main.js
@@ -10,7 +10,11 @@ let mainWindow
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    titleBarStyle: 'hidden'
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000')
